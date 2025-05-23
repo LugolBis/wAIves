50193ee3baee18b8ff4396b02e6a756e2c37dc8f
LugolBis <ld.desmares@gmail.com>
2025-05-23 23:24:31 +0200

    Update training_model.py

--- src/training_model.py
+++ src/training_model.py
@@ -11,38 +11,46 @@ def formatage_data(file_path: str):
     try:
         with open(file_path, 'r') as fs:
             dataset = json.load(fs)
-        inputs = []
-        outputs = []
+        
+        dataset_np = np.array(dataset, dtype=np.float32)
+        if len(dataset_np) % 7 != 0:
+            raise ValueError("The dataset need to be a multiple of 7.")
+        dataset_np = dataset_np.reshape(-1, 7)
 
-        for index in range(0,len(dataset),7):
-            temperature, pressure, winds, windd, waves = dataset[index+2], dataset[index+3], dataset[index+4], dataset[index+5], dataset[index+6]
-            inputs.append(np.array([temperature, pressure, winds, windd], dtype=np.float32))
-            outputs.append(np.array([waves], dtype=np.float32))
+        inputs = dataset_np[:, 2:6]   
+        outputs = dataset_np[:, 6:]
 
-        inputs = np.array(inputs, dtype=np.float32)
-        outputs = np.array(outputs, dtype=np.float32)
-        print("Successfully format the data")
+        print("\nSuccessfully format the data.\n")
         return inputs, outputs
     except Exception as e:
-        print(f"ERROR - inconsistent format : {e}")
+        print(f"\nERROR : {e}\n")
         return None, None
 
 def training_model(data_path: str, model_path: str):
     data_inputs, data_outputs = formatage_data(data_path)
-    if data_inputs is None or data_outputs is None:
-        print("ERROR when try to format the data")
+    if data_inputs is None:
         return
 
     model = tf.keras.Sequential([
-        tf.keras.layers.Dense(128, activation='relu', input_shape=(4,)), 
+        tf.keras.layers.Dense(128, activation='relu', input_shape=(4,)),
         tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.Dense(1)
     ])
 
     model.compile(optimizer='adam', loss='mean_squared_error')
-
-    model.fit(data_inputs, data_outputs, epochs=50)
-
+    
+    # Data pipeline
+    dataset = tf.data.Dataset.from_tensor_slices((data_inputs, data_outputs))
+    dataset = dataset.batch(32).prefetch(tf.data.AUTOTUNE)
+    
+    model.fit(
+        dataset,
+        epochs=50,
+        callbacks=[
+            tf.keras.callbacks.EarlyStopping(monitor="loss", patience=3)
+        ]
+    )
+    
     model.save(model_path)
     print("\nSuccessfully train the model.\n")
 
@@ -52,7 +60,7 @@ if __name__ == '__main__':
     if len(args) == 3:
         training_model(args[1], args[2])
     else:
-        print("Usage : [training_model.py] dataset.json model.h5")
+        print("\nUsage : training_model.py path/to/dataset.json path/to/model.h5")
 
 """
 Requierment :
