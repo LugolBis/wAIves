d1eb037d7103c3fd25ecbd6e997a73d862a85fbe
LugolBis <ld.desmares@gmail.com>
2025-05-24 14:16:04 +0200

    Add the web front-end

---
+++ web/events.js
@@ -0,0 +1,32 @@
+// Management of interactive country selection
+
+const resultsContainer = document.getElementById("autocomplete-results");
+const countryInput = document.getElementById("countryInput");
+
+countryInput.addEventListener("input", searchAbbreviation);
+function searchAbbreviation() {
+    const query = countryInput.value.toLowerCase();
+    resultsContainer.innerHTML = ""; // Clear previous results
+
+    if (query) {
+        const filteredOptions = Object.keys(countriesAbbreviation).filter(option => 
+            option.toLowerCase().includes(query)
+        );
+
+        filteredOptions.forEach(option => {
+            const resultItem = document.createElement("div");
+            resultItem.textContent = option;
+            resultItem.addEventListener("click", function() {
+                countryInput.value = option; // Set input value to the clicked option
+                resultsContainer.innerHTML = ""; // Clear results
+            });
+            resultsContainer.appendChild(resultItem);
+        });
+    }
+}
+
+document.addEventListener("click", function(event) {
+    if (!event.target.closest(".autocomplete-container")) {
+        resultsContainer.innerHTML = ""; // Close dropdown if clicked outside
+    }
+});
