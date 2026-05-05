78da0921206c77b4487bee0fa218a255d51d7394
LugolBis <ld.desmares@gmail.com>
2026-05-05 22:57:36 +0200

    Update docs

--- docs/wasm/app.js
+++ docs/wasm/app.js
@@ -1870,7 +1870,7 @@ export default __wbg_init;
 globalThis.__wasm_split_main_initSync = initSync;
 
 // Actually perform the load
-__wbg_init({module_or_path: "/https://lugolbis.github.io/wAIves/wasm/app_bg.wasm"}).then((wasm) => {
+__wbg_init({module_or_path: "/wAIves/docs/wasm/app_bg.wasm"}).then((wasm) => {
     // assign this module to be accessible globally
     globalThis.__dx_mainWasm = wasm;
     globalThis.__dx_mainInit = __wbg_init;
